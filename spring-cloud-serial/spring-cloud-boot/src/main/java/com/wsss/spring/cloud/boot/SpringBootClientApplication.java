a86e76795f84e0482b14abd760997ff863ecd972
hasee <hasee@172.28.14.16>
2019-06-14 09:01:20 +0800

    123

--- spring-cloud-serial/spring-cloud-boot/src/main/java/com/wsss/spring/cloud/boot/SpringBootClientApplication.java
+++ spring-cloud-serial/spring-cloud-boot/src/main/java/com/wsss/spring/cloud/boot/SpringBootClientApplication.java
@@ -1,29 +1,44 @@
 package com.wsss.spring.cloud.boot;
 
-import org.springframework.beans.factory.annotation.Value;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @SpringBootApplication
 @RestController
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 public class SpringBootClientApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(SpringBootClientApplication.class, args);
-		 new SpringApplicationBuilder(SpringBootClientApplication.class)
-			.properties("spring.config.location=classpath:/application.yml").run(args);
+		// SpringApplication.run(SpringBootClientApplication.class, args);
+		new SpringApplicationBuilder(SpringBootClientApplication.class)
+				.properties("spring.config.location=classpath:/application.yml").run(args);
 	}
 
-	@Value("${hello}")
-	String hello;
+	private String hello = "hello";
 
 	@RequestMapping(value = "/hello")
-	public String hello() {
+	public String hello() throws InterruptedException {
+		System.out.println("ok");
+		return hello;
+	}
+
+	@RequestMapping(value = "/redirect")
+	public RedirectView redirect() throws InterruptedException {
+		System.out.println("redirect");
+		// Thread.sleep(5000);
+		return new RedirectView("/spring-boot-client/hello", true);
+	}
+
+	@RequestMapping(value = "/**")
+	public String hello2(HttpServletRequest request) throws InterruptedException {
+		System.out.println(request.getRequestURI());
 		return hello;
 	}
 }
